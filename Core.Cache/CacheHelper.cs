acef4bf0fc6ee929ae4badcad7bcc91bb4c9b59a
puling <oaketree@163.com>
2016-06-06 10:18:00 +0800

    unity

---
+++ Core.Cache/CacheHelper.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Web;
+using System.Web.Caching;
+
+namespace Core.Cache
+{
+    public class CacheHelper
+    {
+        /// <summary>
+        /// 本地缓存获取
+        /// </summary>
+        /// <param name="name">key</param>
+        /// <returns></returns>
+        public static object Get(string name)
+        {
+            return HttpRuntime.Cache.Get(name);
+        }
+
+        /// <summary>
+        /// 本地缓存移除
+        /// </summary>
+        /// <param name="name">key</param>
+        public static void Remove(string name)
+        {
+            if (HttpRuntime.Cache[name] != null)
+                HttpRuntime.Cache.Remove(name);
+        }
+
+        /// <summary>
+        /// 本地缓存写入（默认缓存20min）
+        /// </summary>
+        /// <param name="name">key</param>
+        /// <param name="value">value</param>
+        public static void Set(string name, object value)
+        {
+            Set(name, value, null);
+        }
+        /// <summary>
+        /// 本地缓存写入（默认缓存20min）,依赖项
+        /// </summary>
+        /// <param name="name">key</param>
+        /// <param name="value">value</param>
+        /// <param name="cacheDependency">依赖项</param>
+        public static void Set(string name, object value, CacheDependency cacheDependency)
+        {
+            HttpRuntime.Cache.Insert(name, value, cacheDependency, System.Web.Caching.Cache.NoAbsoluteExpiration, TimeSpan.FromMinutes(20));
+        }
+
+        /// <summary>
+        /// 本地缓存写入
+        /// </summary>
+        /// <param name="name">key</param>
+        /// <param name="value">value</param>
+        /// <param name="minutes">缓存分钟</param>
+        public static void Set(string name, object value, int minutes)
+        {
+            HttpRuntime.Cache.Insert(name, value, null, System.Web.Caching.Cache.NoAbsoluteExpiration, TimeSpan.FromMinutes(minutes));
+        }
+    }
+}
---
+++ Core.Cache/GetConn.cs
@@ -0,0 +1,25 @@
+﻿using System.Configuration;
+
+namespace Core.Cache
+{
+    public class GetConn
+    {
+        private string key;
+        public GetConn(string key)
+        {
+            this.key = key;
+        }
+        public string Conn()
+        {
+            var conn = CacheHelper.Get(key);
+            if (conn != null)
+                return conn.ToString();
+            else
+            {
+                conn = ConfigurationManager.ConnectionStrings[key].ConnectionString;
+                CacheHelper.Set(key, conn);
+                return conn.ToString();
+            }
+        }
+    }
+}
