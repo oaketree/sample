ebe8e24e69d17f4f9f8512cc007e710cf3017157
puling <oaketree@163.com>
2016-06-15 09:33:36 +0800

    615

---
+++ Cms.BLL/Common/Services/Menu.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cms.BLL.Common.Services
+{
+    public class Menu
+    {
+        public int MenuID { get; set; }
+        public int PID { get; set; }
+        public string MenuName { get; set; }// 菜单名称
+        public string Url { get; set; }// URL地址
+        public List<Menu> Childs { get; set; }//子菜单
+    }
+    public class HandleMenu
+    {
+        public List<Menu> handleSubMenu(List<Menu> menuItem,int topid)
+        {
+            var lm = new List<Menu>();
+            foreach (var item in menuItem)
+            {
+
+                if (item.PID == topid)
+                {
+                    var m = new Menu();
+                    m.MenuID = item.MenuID;
+                    m.PID = item.PID;
+                    m.MenuName = item.MenuName;
+                    m.Url = item.Url;
+                    var hsm = handleSubMenu(menuItem, item.MenuID);
+                    if (hsm.Count > 0)
+                        m.Childs = hsm;
+                    else
+                        m.Childs = null;
+                    lm.Add(m);
+                    //lm.Add(new Menu
+                    //{
+                    //    MenuID = item.MenuID,
+                    //    PID = item.PID,
+                    //    MenuName = item.MenuName,
+                    //    Url = item.Url,
+                    //    Childs = handleSubMenu(menuItem, item.MenuID)
+                    //});
+                }
+            }
+            return lm;
+        }
+    }
+   
+}
---
+++ Cms.BLLTest/Common/Services/HandleMenuTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Cms.BLL.Common.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Cms.BLL.Common.Services.Tests
+{
+    [TestClass()]
+    public class HandleMenuTests
+    {
+        [TestMethod()]
+        public void handleSubMenuTest()
+        {
+            var a = new List<Menu>();
+            a.Add(new Menu { MenuID = 1, PID = 0, MenuName = "a", Url = "ua" });
+            a.Add(new Menu { MenuID = 2, PID = 0, MenuName = "b", Url = "ub" });
+            a.Add(new Menu { MenuID = 3, PID = 0, MenuName = "c", Url = "uc" });
+            a.Add(new Menu { MenuID = 4, PID = 1, MenuName = "d", Url = "ud" });
+            a.Add(new Menu { MenuID = 5, PID = 2, MenuName = "e", Url = "ue" });
+            a.Add(new Menu { MenuID = 6, PID = 3, MenuName = "f", Url = "uf" });
+            a.Add(new Menu { MenuID = 7, PID = 4, MenuName = "g", Url = "uf" });
+            a.Add(new Menu { MenuID = 8, PID = 5, MenuName = "h", Url = "uf" });
+            a.Add(new Menu { MenuID = 9, PID = 6, MenuName = "i", Url = "uf" });
+            a.Add(new Menu { MenuID = 10, PID = 9, MenuName = "j", Url = "uj" });
+            a.Add(new Menu { MenuID = 11, PID = 6, MenuName = "l", Url = "uk" });
+
+            var b = new HandleMenu().handleSubMenu(a,0);
+            Console.Write(JsonConvert.SerializeObject(b));
+
+        }
+    }
+}
